5e5846e148e44b85b7960fb3450d0fbc37e27727
Aluno Grau <alunograu@tecnico.com>
2026-02-09 09:19:38 -0300

    Aula 2026_02_09 - Second Commit

--- Aula 02/Atividade01.py
+++ Aula 02/Atividade01.py
@@ -1,5 +1,5 @@
 class Client:
-    def __init__(self, id_client, name):
+    def __init__(self, id_client:str, name:str):
         self.id = id_client
         self.name = name
         
@@ -8,9 +8,34 @@ class Client:
             return NotImplemented
         return self.id == other.id
     
-#Uso
-client1 = Client("001", "Ana Silva")
-client2 = Client("001", "Ana Paula Silva")
-client3 = Client("002", "Ana Paula Silva")
-print(client1 == client2)
-print(client2 == client3)
+class Price:
+    def __init__(self, value, coin="BRL"):
+        if not isinstance(value, (int,float)) or value <= 0:
+            raise ValueError("Value must be numeric or positive.")
+        self._value = value
+        self._coin = coin
+        
+    @property
+    def value(self):
+        return self._value
+    
+    @property
+    def coin(self):
+        return self._coin
+    
+    def __eq__(self, other):
+        if not isinstance(other, Price):
+            return NotImplemented
+        return self.value == other.value and self.coin == other.coin
+    
+    def __hash__(self):
+        return hash((self.value, self.coin))
+    
+book_price = Price(59.90)
+other_price = Price(59.90)
+new_price = Price(49.90)
+print(book_price == other_price)
+print(book_price == new_price)
+print(hash(book_price))
+print(hash(other_price))
+print(hash(new_price))
