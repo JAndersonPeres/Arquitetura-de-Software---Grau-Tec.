60063cd3d970aef70290c0f20bfecd587910ba5f
Anderson Peres <jpdandersonalencar@gmail.com>
2026-02-27 16:04:06 -0300

    Creation Exceptions - 2026_02_27

---
+++ Projeto Final - Extra/domain/exception/CourseNotFoundException.py
@@ -0,0 +1,3 @@
+class CourseNotFoundException(Exception):
+    def __init__(self, message:str):
+        super().__init__(message)
---
+++ Projeto Final - Extra/domain/exception/StudentNotFoundException.py
@@ -0,0 +1,3 @@
+class StudentNotFoundException(Exception):
+    def __init__(self, message:str):
+        super().__init__(message)
